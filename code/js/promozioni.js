7ad001db7fdeba1695b1705a52832edb535f67d3
cimbo92 <alecimbo92@gmail.com>
2016-06-09 18:31:36 +0200

    Dynamic link product page

--- code/js/promozioni.js
+++ code/js/promozioni.js
@@ -1,2 +1,15 @@
-//select the right text in the menu
-$('#menu_promozioni').addClass('current');
+
+//// the following 3 rows are shared in the js of each page with different parameters
+//load the shared menu 
+$("#header").load("shared-menu.html");
+//load the shared footer
+$("#footer").load("shared-footer.html");
+
+
+//async ajax request
+$("document").ready(function(){
+    
+    //select the right text in the menu
+    $('#menu_promozioni').addClass('current');
+
+});
