01d32b8eebc8a58f8e5e3a7e9860df79ddae01a9
matteo_daverio <matteo.daverio@mail.polimi.it>
2016-06-15 16:16:28 +0200

    footer experiment

--- code/js/menu.assistance_service.js
+++ code/js/menu.assistance_service.js
@@ -1,2 +1,17 @@
 //when page is complete select the current item of the menu
-$("document").ready(function(){ $('#menu_assistenza').addClass('current');});
+$("document").ready(function() { 
+    $('#menu_assistenza').addClass('current');
+    placeFooter();
+});
+
+// position the footer in the end of the page
+function placeFooter() {
+    $('#footer').css({ 'margin-top': 0 });
+    var windowH = $(window).height();
+    var wrapperH = $('#header').height() + $('#page_title').height() + $('#content').height();
+    if( windowH > wrapperH ) {
+        $('#footer').css({ 'margin-top': ( windowH - wrapperH ) });        
+    }
+    //when page is complete select the current item of the menu
+    $("document").ready(function(){ $('#menu_highlights').addClass('current');}); 
+}
